396b288ddd2d6dc223dbebed0dfd295eb7e004c5
Daniel <angelicodaniel93@gmail.com>
2023-01-26 14:08:47 +0100

    first commit

---
+++ app/Http/Controllers/Admin/PostController.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data = [
+            'posts' => Post::paginate(10)
+        ];
+
+
+        return view('admin.posts.index', $data);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.posts.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->all();
+
+        //validate
+        $request->validate([
+            'title' => 'required',
+            'body' => 'required'
+        ]);
+
+
+        $newPost = new Post();
+        $newPost->fill($data);
+        $newPost->save();
+
+        return redirect()->route('admin.posts.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $singolo_post = Post::findOrFail($id);
+        return view('admin.posts.show', compact('singolo_post'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $post = Post::findOrFail($id);
+
+        return view('admin.posts.edit', compact('post'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $data = $request->all();
+        $singolo_post = Post::findOrFail($id);
+
+        $singolo_post->update($data);
+
+        return redirect()->route('admin.posts.show', $singolo_post->id);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $singoloPost = Post::findOrFail($id);
+        $singoloPost->delete();
+
+        return redirect()->route('admin.posts.index');
+    }
+}
---
+++ resources/js/components/WorkInProgress.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1>Work In Progress (componente)</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'WorkInProgress'
+}
+</script>
